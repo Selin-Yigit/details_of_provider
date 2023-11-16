9e95117bfc8252918ba6fbd567f5c0ddbc85bf51
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 22:27:43 +0300

    ExpensiveWidget oluşturuldu.

---
+++ lib/expensiveWidget.dart
@@ -0,0 +1,42 @@
+import 'package:details_of_provider/expensiveObjectClass.dart';
+import 'package:details_of_provider/objectProvider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ExpensiveWidget extends StatelessWidget {
+  const ExpensiveWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //select için ilk referans girdi, ikinci referans ise çıktı (return)
+    // yani bu durumda bir provider girdi ve expensiveObject return edildi.
+
+    final expensiveObject = context.select<ObjectProvider, ExpensiveObject>(
+      (provider) => provider.expensiveObject,
+    );
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.amberAccent.shade200,
+      child: Column(
+        children: [
+          const Center(
+            child: Text(
+              "Expensive Widget",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Text("Last Updated: "),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(expensiveObject.lastUpdated),
+          ),
+        ],
+      ),
+    );
+  }
+}
