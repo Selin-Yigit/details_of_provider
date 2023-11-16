38095510c1c25ff5d14bf69b0f8406534204d52e
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 22:31:16 +0300

    CheapWidget oluşturuldu.

--- lib/cheapWidget.dart
+++ lib/cheapWidget.dart
@@ -1,10 +1,39 @@
+import 'package:details_of_provider/cheapObjectClass.dart';
+import 'package:details_of_provider/objectProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CheapWidget extends StatelessWidget {
   const CheapWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final cheapObject = context.select<ObjectProvider, CheapObject>(
+      (provider) => provider.cheapObject,
+    );
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.blueAccent.shade200,
+      child: Column(
+        children: [
+          const Center(
+            child: Text(
+              "Cheap Widget",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Text("Last Updated: "),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(cheapObject.lastUpdated),
+          ),
+        ],
+      ),
+    );
   }
 }
