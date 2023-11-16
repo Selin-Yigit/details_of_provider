cba794a3554b9439a8ed72eac715ab2928961426
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 21:35:08 +0300

    HomePage oluşturuldu.

---
+++ android/app/src/main/kotlin/com/example/details_of_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.details_of_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent.shade200,
+        title: const Text(
+          "Homepage",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
