887c89b6588d16c583931ac418220694433b52aa
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 21:50:48 +0300

    Expensive ve Cheap Object Class'lar oluşturuldu.

---
+++ lib/cheapObjectClass.dart
@@ -0,0 +1,5 @@
+import 'package:details_of_provider/baseObjectClass.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class CheapObject extends BaseObject {}
---
+++ lib/expensiveObjectClass.dart
@@ -0,0 +1,7 @@
+import 'package:details_of_provider/baseObjectClass.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class ExpensiveObject extends BaseObject{
+  
+}
